ed3d162cf742b640c98a0f254756bea42fd418a4
merkulovvs <mervic81@gmail.com>
2017-04-18 10:38:35 +0300

    Initial commit

---
+++ src/module1/Assignment01_1.java
@@ -0,0 +1,19 @@
+package module1;
+import java.util.Scanner;
+
+/**
+ * Created by merkulovvs on 4/18/2017.
+ */
+public class Assignment01_1 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+
+        String name = in.next();
+
+        System.out.println("Hello, " + name);
+    }
+}
