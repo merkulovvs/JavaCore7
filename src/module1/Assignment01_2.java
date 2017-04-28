9db9d6d6f7bdaaee091ea6c2f1ea6502c390ecb3
merkulovvs <mervic81@gmail.com>
2017-04-28 22:58:01 +0300

    module 1 homework, assignment 2 - загуглил пример сортировки пузырьком и применил его в своём коде.

--- src/module1/Assignment01_2.java
+++ src/module1/Assignment01_2.java
@@ -20,11 +20,11 @@ public class Assignment01_2 {
         int temp;
         for(int i = 0; i < array.length; i++)
             for(int j = 1; j < (array.length - i); j++){
-               if(array[j - 1] > array[j]){
-                   temp = array[j-1];
-                   array[j-1] = array[j];
-                   array[j] = temp;
-               }
+                if(array[j - 1] > array[j]){
+                    temp = array[j-1];
+                    array[j-1] = array[j];
+                    array[j] = temp;
+                }
             }
 
         System.out.println("Sorted array: " + Arrays.toString(array));
